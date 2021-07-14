822f81d493b760bbdceb85b673f920cafabd7dc5
Alexander <kosenkov.alexander.it@gmail.com>
2021-07-14 23:46:29 +1100

    Documenting for task_1 added

--- task_1.py
+++ task_1.py
@@ -2,6 +2,12 @@
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 
 def func_div(user_number_1, user_number_2):
+    '''
+    Функция делит первый аргумент на второй
+    :param user_number_1: int, первый аргумент
+    :param user_number_2: int, второй аргумент
+    :return: result, int, результат деления двух аргументов
+    '''
     while True:
         user_number_1 = int(input('Введите число: '))
         user_number_2 = int(input('Введите число: '))
