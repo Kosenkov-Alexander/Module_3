9a2ae23530c4328a632a693aed97d5fa8a960ac1
Alexander <kosenkov.alexander.it@gmail.com>
2021-07-14 23:34:55 +1100

    task_1 done

--- task_1.py
+++ task_1.py
@@ -1,1 +1,16 @@
-print('Hello')
+# 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
+# Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+
+def func_div(user_number_1, user_number_2):
+    while True:
+        user_number_1 = int(input('Введите число: '))
+        user_number_2 = int(input('Введите число: '))
+        if user_number_2 == 0:
+            print('Вы пытаетесь поделить на ноль')
+            continue
+        else:
+            break
+    result = user_number_1 / user_number_2
+    return result
+
+print(func_div(10, 5))
